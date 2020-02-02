fad590bc42978993973b41401a4d3da19eb0ea6f
ismail shahzad <ismail.shahzad@datopian.com>
2020-02-03 03:08:55 +0500

    first commit

---
+++ ckanext/ckan_cluster/actions.py
@@ -0,0 +1,86 @@
+import ckan.plugins as plugins
+from ckan import authz
+from pylons import config
+import ckan.plugins.toolkit as toolkit
+from ckan.common import _ 
+import jenkins
+import re
+import logging
+
+#token = '110821fb6940fe4501e8cdc63f22b62ad0'
+user = 'ismail.shahzad@datopian.com'
+server_port = 'https://cc-p-jenkins.ckan.io'
+
+jenkins_key = config.get('ckan.ckan_cluster_jenkins_key')
+jenkins_key = str(jenkins_key)
+logging.error('******************************')
+logging.error(jenkins_key)
+
+logging.error('******************************')
+logging.error(config.get('ckan.harvest.mq.redis_db'))
+logging.error('*******************************')
+logging.error(config)
+
+@toolkit.side_effect_free
+def active_instances(context, data_dict):
+    '''Returns a list of active instances of ckan'''
+
+    if not authz.is_sysadmin(toolkit.c.user):
+        toolkit.abort(403, _('You are not authorized to access this list'))
+    
+    server = jenkins.Jenkins(server_port, username=user, 
+                         password=jenkins_key)
+    job_name = 'simplified list instances' 
+
+
+    # Get the console output of the latest build of the job 
+    info = server.get_job_info(job_name)
+    builds = info['builds']
+    number = int(builds[-1]['number'])
+    console_output = server.get_build_console_output(job_name, number)
+    
+    data_split = console_output.split('\n')
+    
+    url_list = []
+    active_instances = []
+
+    # Go through the data line by line and search for id,repo and routes
+    # and append them in active_instances
+    for line in data_split:
+        line = line.strip()  
+        instance_id = re.search('== (.*)', line)
+        git_repo = (re.search('registry.(.*)@', line) or
+        re.search('imageFromGitlab: (.*)',line))
+        url = None
+
+        if line.startswith('-'):
+            url = re.search('- (.*)', line)
+
+        if instance_id:
+            instance_rep = instance_id.group(1)       
+            if url_list:
+                active_instances.append({
+                    'id': instance_id_data,
+                    'config_repo':repo, 
+                    'instance_url':url_list
+                     })
+                url_list = []
+        if git_repo:
+            repo = 'https://'+ git_repo.group(1)
+        if url:
+            route = 'https://'+ url.group(1)
+            url_list.append(route)
+            instance_id_data = instance_rep
+    
+    active_instances.append(
+        {
+            'id':instance_id_data,
+            'config_repo':repo,
+            'instance_url':url_list
+        }
+    )
+
+    return active_instances
+
+
+
---
+++ ckanext/ckan_cluster/plugin.py
@@ -0,0 +1,26 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+from ckanext.ckan_cluster import actions
+
+
+class Ckan_ClusterPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IActions)
+
+    # IConfigurer
+    # NEW
+
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('fanstatic', 'ckan_cluster')
+
+    # IActions
+    def get_actions(self):
+        '''
+        Define custom functions (or ovveride existing ones).
+        Availbale via API /api/action/{action-name}
+        '''
+        return {
+            'active_instances': actions.active_instances,
+           }
