43365d06fbaeffd4b56bbd3c00ebf348737670a5
MuhammadIsmailShahzad <57398621+MuhammadIsmailShahzad@users.noreply.github.com>
2020-02-21 20:35:55 +0500

    [actions.py] Added a function for writing instances to a gsheet

--- ckanext/ckan_cluster/actions.py
+++ ckanext/ckan_cluster/actions.py
@@ -4,12 +4,27 @@ from pylons import config
 import ckan.plugins.toolkit as toolkit
 from ckan.common import _ 
 import jenkins
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 import re
 import logging
+import time
 
 user = config.get('ckan.jenkins_user')
 server_port = config.get('ckan.jenkins_server_port')
 jenkins_key = config.get('ckan.jenkins_token')
+
+# use creds to create a client to interact with the Google Drive API 
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+creds = ServiceAccountCredentials.from_json_keyfile_name('/srv/app/client_secret.json', scope)
+client = gspread.authorize(creds)
+sheet_key = config.get('ckan.gsheet_id')
+worksheet_id = config.get('ckan.gsheet_worksheet')
+gsheet = client.open_by_key(sheet_key)
+sheet = gsheet.get_worksheet(int(worksheet_id))
+
 
 @toolkit.side_effect_free
 def active_instances(context, data_dict):
@@ -71,3 +86,31 @@ def active_instances(context, data_dict):
 
     return active_instances
 
+@toolkit.side_effect_free
+def update_gsheet(context, data_dict):
+    '''Writes the list of instances to a googlesheet 
+    https://docs.google.com/spreadsheets/d/{sheet_key}/{worksheet_id}'''
+    active_instances_obs = active_instances(context, data_dict) 
+    
+    sheet.clear()
+    row = 2
+    column = 1
+    request_count = 0
+    #limit for write request per 100 seconds
+    writeQuota = 80 
+    header = ['id','config_repo','url_routes']
+    sheet.insert_row(header, 1) 
+    write_row = []
+
+    for x in active_instances_obs:
+        if(request_count == writeQuota):
+            time.sleep(30)
+            request_count = 0
+        write_row.append(str(x['id']))
+        write_row.append(str(x['config_repo']))
+        write_row.append(str(x['instance_url']).replace('u\'',' ').replace('\'',''))
+        sheet.insert_row(write_row, row)
+        row += 1
+        request_count += 1
+        write_row = []
+    return u'https://docs.google.com/spreadsheets/d/{}/edit#gid={}'.format(sheet_key, worksheet_id)
