6ccaa3c9ef6c15ca5f2f1ec6899124048bcf2d91
ismail shahzad <ismail.shahzad@datopian.com>
2020-03-29 23:50:20 +0500

    [actions.py] Fixes the issue of wrong config repos being assigned to some instances

--- ckanext/ckan_cluster/actions.py
+++ ckanext/ckan_cluster/actions.py
@@ -18,9 +18,7 @@ server_port = config.get('ckan.jenkins_server_port')
 jenkins_key = config.get('ckan.jenkins_token')
 
 
-logging.error(config)
-
-# use creds to create a client to interact with the Google Drive API 
+# use creds to create a client to interact with the Google Drive API
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 
@@ -31,49 +29,52 @@ worksheet_id = config.get('ckan.gsheet_worksheet')
 sheet_name = config.get('ckan.gsheet_name')
 gsheet = client.open_by_key(sheet_key)
 sheet = gsheet.get_worksheet(int(worksheet_id))
-logging.error('GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG')
-logging.error(sheet_name)
+
 
 @toolkit.side_effect_free
 def active_instances(context, data_dict):
     '''Returns a list of active instances of ckan'''
-    
+
     if not authz.is_sysadmin(toolkit.c.user):
         toolkit.abort(403, _('You are not authorized to access this list'))
-    
-    server = jenkins.Jenkins(server_port, username=user, 
-                         password=jenkins_key)
-    job_name = 'simplified list instances' 
 
-    # Get the console output of the latest build of the job 
+    server = jenkins.Jenkins(server_port, username=user,
+                         password=jenkins_key)
+    job_name = 'simplified list instances'
+
+    # Get the console output of the latest build of the job
     info = server.get_job_info(job_name)
     builds = info['builds']
     number = int(builds[0]['number'])
     console_output = server.get_build_console_output(job_name, number)
-    
+
     data_split = console_output.split('\n')
-    
+
     url_list = []
     active_instances = []
 
     # Go through the data line by line and search for id,repo and routes
     # and append them in active_instances
     for line in data_split:
-        line = line.strip()  
+        line = line.strip()
         instance_id = re.search('== (.*)', line)
-        git_repo = (re.search('registry.(.*)@', line) or
-        re.search('imageFromGitlab: (.*)',line))
+        git_repo = (re.search('imageFromGitlab: (.*)',line) or
+        re.search('registry.(.*)', line))
+
+        if (re.search('registry.(.*)@', line)):
+            git_repo = re.search('registry.(.*)@', line)
+
         url = None
 
         if line.startswith('-'):
             url = re.search('- (.*)', line)
 
         if instance_id:
-            instance_rep = instance_id.group(1)       
+            instance_rep = instance_id.group(1)
             if url_list:
                 active_instances.append({
                     'id': instance_id_data,
-                    'config_repo':repo, 
+                    'config_repo':repo,
                     'instance_url':url_list
                      })
                 url_list = []
@@ -83,7 +84,7 @@ def active_instances(context, data_dict):
             route = 'https://{}'.format(url.group(1))
             url_list.append(route)
             instance_id_data = instance_rep
-    
+
     active_instances.append(
         {
             'id':instance_id_data,
@@ -97,12 +98,12 @@ def active_instances(context, data_dict):
 @toolkit.side_effect_free
 def update_gsheet(context, data_dict):
 
-    '''Writes the list of instances to a googlesheet 
+    '''Writes the list of instances to a googlesheet
     https://docs.google.com/spreadsheets/d/{sheet_key}/{worksheet_id}'''
-    
+
     if authz.is_sysadmin(toolkit.c.user):
-        active_instances_obs = active_instances(context, data_dict) 
-    
+        active_instances_obs = active_instances(context, data_dict)
+
         sheet.clear()
         #header = ['id','config_repo','url_routes']
         write_row = []
@@ -111,14 +112,14 @@ def update_gsheet(context, data_dict):
         #new_list.append(header)
 
         for x in active_instances_obs:
-            
+
             write_row.append(str(x['id']))
             write_row.append(str(x['config_repo']))
 
             count = 0
-            
+
             for url in x['instance_url']:
-                
+
                 write_row.append(str(url))
                 count += 1
 
@@ -127,10 +128,10 @@ def update_gsheet(context, data_dict):
 
             new_list.append(write_row)
             write_row = []
-        
+
         header = ['id','config_repo'] + [u'route no. {0}'.format(i) for i in range(1,prev_count+1)]
-        new_list.insert(0,header)        
-        
+        new_list.insert(0,header)
+
         gsheet.values_update(
             sheet_name+'!A1',
             params={
@@ -142,12 +143,12 @@ def update_gsheet(context, data_dict):
         )
     csv_file = create_dataset_csv(active_instances_obs)
     user_list = ckan.logic.get_action('user_list')(context, data_dict)
-    
+
     user_name_list = [user['name'] for user in user_list]
 
     if toolkit.c.user not in user_name_list:
         toolkit.abort(403, _('You are not authorized to access this list'))
-    
+
     dataset_id = 'list-instances-dataset'
     owner_org_id = 'operations'
     resource_id = 'active-instances.csv'
@@ -156,14 +157,14 @@ def update_gsheet(context, data_dict):
     resource_url = 'https://raw.githubusercontent.com/MuhammadIsmailShahzad/telecom-operators-of-the-world/master/telecom-operators.csv'
 
     not_found_error = ckan.logic.NotFound
-     
+
     try:
         result = toolkit.get_action('package_show')(context, {'id': dataset_id})
-    
+
     except(not_found_error):
-    
+
         result = toolkit.get_action('package_create')(context, {'name': dataset_id,'owner_org': owner_org_id })
-    result2 = []      
+    result2 = []
     if resource_id not in result['resources']:
         #if resource == resource_id:
         #    result2 = resource['name']
@@ -171,7 +172,7 @@ def update_gsheet(context, data_dict):
         result2 = u'Please create a resource'
         result2 = toolkit.get_action('resource_create')(context, {'package_id':dataset_id,'upload': csv_file})
         #break
-    
+
     try:
         result1 = toolkit.get_action('resource_show')(context, {'id': resource_id})
     except:
@@ -194,4 +195,4 @@ def create_dataset_csv(active_instances_obs):
                 writer.writerow(data)
     except IOError:
         print("I/O error")
-    return csv_file 
+    return csv_file
